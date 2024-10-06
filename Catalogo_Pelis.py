f82bd47180bd9a344c8d4a7c06796395fda8de93
nahir28 <nahirleyes67@gmail.com>
2024-10-05 22:59:30 -0300

    subiendo segunda parte codigo

--- Catalogo_Pelis.py
+++ Catalogo_Pelis.py
@@ -1,3 +1,5 @@
+import os #modulo para poder eliminar archivo
+
 class Pelicula:             #definimos una clase con atributo privado
     def __init__(self, nombre, director, genero):
         self.__nombre = nombre
@@ -18,4 +20,48 @@ class Pelicula:             #definimos una clase con atributo privado
     def __str__(self):
         return f"{self.__nombre} Dirigida por: {self.director} Género: {self.genero}"   #retornamos los datos de nuestra clase
     
-    import os #modulo para poder eliminar archivo
+
+    
+class CatalogoPelicula:     #Definimos la clase de catalogo de peliculas 
+    ruta_archivo = "Pelis.txt"      #definimos la variable con el nombre de donde se almacenara el catalogo de pelicula
+    
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.ruta_archivo = f"{self.nombre}.txt"
+        self.peliculas = self.cargar_peliculas()
+
+    def cargar_peliculas(self):
+        peliculas = []
+        try:
+            with open(self.ruta_archivo, 'r') as archivo:
+                for linea in archivo:
+                    nombre, director, genero = linea.strip().split(',')     #utilizamos algunos mmetodos str para mejorar el orden de escritura
+                    peliculas.append(Pelicula(nombre, director, genero))
+        except FileNotFoundError:
+            print(f"El archivo '{self.ruta_archivo}' no existe. Se creará un nuevo catálogo.")
+        return peliculas
+
+    def guardar_peliculas(self):
+        with open(self.ruta_archivo, 'w') as archivo:       #abrimos el archivo y escribimos los datos de cada pelicula
+            for pelicula in self.peliculas:
+                archivo.write(f"{pelicula.nombre},{pelicula.director},{pelicula.genero}\n")
+
+    def agregar(self, pelicula):
+        self.peliculas.append(pelicula)     #abrimos el archivo y agregamos la pelicula
+        self.guardar_peliculas()
+
+    def listar(self):
+        if not self.peliculas:
+            print("El catálogo está vacío.")
+        else:
+            for pelicula in self.peliculas:
+                print(pelicula)
+
+    def eliminar(self):
+        try:
+            os.remove(self.ruta_archivo)        #utilizamos os.remove para eliminar el archivo
+            self.peliculas = []
+            print(f"El catálogo '{self.nombre}' fue eliminado.")
+        except FileNotFoundError:     
+            print(f"El catálogo '{self.nombre}' no existe.")
+            
